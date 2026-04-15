f8b82da6e1050a39977d192da775d52300eb3ace
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-04-15 16:57:28 +0200

    comments and change of visibility to public

--- domain/Product.java
+++ domain/Product.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Product {
-    private String name;
-    private List<Material> materials = new ArrayList<>();
+    public String name;
+    public List<Material> materials = new ArrayList<>();
 
     public Product(String name) {
         this.name = name; // Basic constructor for the skeleton
     }
+
+    // calculateImpact()
+
+    // recyclingGuidance()
 
 }
--- domain/RecyclingGuidance.java
+++ domain/RecyclingGuidance.java
@@ -1,5 +1,6 @@
 package domain;
 
 public class RecyclingGuidance {
-    
+    // This will probably have a if statement using some instanceOf function to determine the instructions returned to the end user
+    public String instructions = "";
 }
