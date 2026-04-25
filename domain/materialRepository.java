772eee81378996472abf4429eb5d09b1297cd40a
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-04-25 15:38:13 +0200

    Capital letter for MaterialRepo

--- domain/materialRepository.java
+++ domain/materialRepository.java
@@ -1,7 +1,7 @@
 package domain;
 import java.util.List;
 
-public interface materialRepository {
+public interface MaterialRepository {
     void save(Material material);
     List<Material> findAll();
 }
