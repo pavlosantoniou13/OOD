2c8d0e219186b5ca4defbda1e7895f0e86e6baa8
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-04-25 15:04:22 +0200

    Update ProductRepo
    Adds findById

--- domain/ProductRepository.java
+++ domain/ProductRepository.java
@@ -1,8 +1,11 @@
 package domain;
 
 import java.util.List;
+import java.util.UUID;
+
 
 public interface ProductRepository {
     void save(Product product);
     List<Product> findAll();
+    Product findById(UUID id);
 }
