9f4819584f42ba695fcf0ea2a486c206f767bbab
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-04-26 20:02:34 +0200

    Capitals

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import application.*;
 import presentation.*;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         var repository = new InMemoryProductRepository();
         var service = new ProductService(repository);
