0dab2c24dadb19f116d6d8937dfa746791409442
Pavlos Antoniou <117662861+pavlosantoniou13@users.noreply.github.com>
2026-04-26 20:08:01 +0200

    Update WeightedStrategy
    Implements ImpactStrategy interface

--- application/WeightedStrategy.java
+++ application/WeightedStrategy.java
@@ -1,5 +1,14 @@
 package application;
 
-public class WeightedStrategy {
+import domain.ImpactStrategy;
+import domain.Product;
+
+public class WeightedStrategy implements ImpactStrategy {
+
+    @Override
+    public double calculate(Product product) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+    }
     // Algo
 }
